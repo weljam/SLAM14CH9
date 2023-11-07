5929d59bdbe04fabf537196dbe67ec6db8c459ed
chenweijian <chenweljian@qq.com>
2023-11-07 22:12:10 +0800

    second

--- random.h
+++ random.h
@@ -11,6 +11,11 @@ inline double RandDouble()
     return r / RAND_MAX;
 }
 
+/*利用均匀分布构造正态分布N(0,1)随机点
+Marsaglia polar method算法：  得到满足N（0,1）正态分布的随机点
+  和y是圆： x*x+y*y <=1内的随机点
+  RandNormal()处理后  x*w是服从标准正态分布的样本
+*/
 inline double RandNormal()
 {
     double x1, x2, w;
