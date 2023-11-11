26e7eec55ece6fc71b4a8ee52f0bd8bba1601763
chenweijian <chenweljian@qq.com>
2023-11-11 17:20:26 +0800

    second

--- bundle_adjustment_g2o.cpp
+++ bundle_adjustment_g2o.cpp
@@ -28,6 +28,7 @@ struct PoseAndIntrinsics {
 
     /// 将估计值放入内存
     void set_to(double *data_addr) {
+        //对数映射
         auto r = rotation.log();
         for (int i = 0; i < 3; ++i) data_addr[i] = r[i];
         for (int i = 0; i < 3; ++i) data_addr[i + 3] = translation[i];
@@ -35,7 +36,7 @@ struct PoseAndIntrinsics {
         data_addr[7] = k1;
         data_addr[8] = k2;
     }
-
+    //参数
     SO3d rotation;
     Vector3d translation = Vector3d::Zero();
     double focal = 0;
@@ -45,14 +46,16 @@ struct PoseAndIntrinsics {
 /// 位姿加相机内参的顶点，9维，前三维为so3，接下去为t, f, k1, k2
 class VertexPoseAndIntrinsics : public g2o::BaseVertex<9, PoseAndIntrinsics> {
 public:
+    //确保为Eigen类型正确对齐内存分配。
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
     VertexPoseAndIntrinsics() {}
-
+    //重置
     virtual void setToOriginImpl() override {
         _estimate = PoseAndIntrinsics();
     }
 
+    //更新
     virtual void oplusImpl(const double *update) override {
         _estimate.rotation = SO3d::exp(Vector3d(update[0], update[1], update[2])) * _estimate.rotation;
         _estimate.translation += Vector3d(update[3], update[4], update[5]);
@@ -61,12 +64,16 @@ public:
         _estimate.k2 += update[8];
     }
 
-    /// 根据估计值投影一个点
+    /// 根据估计值投影一个点，Xc = R*Xw+t
+    //归一化：Xc/Xc[2];计算像素坐标
     Vector2d project(const Vector3d &point) {
         Vector3d pc = _estimate.rotation * point + _estimate.translation;
         pc = -pc / pc[2];
+        //归一化坐标的二范数
         double r2 = pc.squaredNorm();
+        //畸变参数
         double distortion = 1.0 + r2 * (_estimate.k1 + _estimate.k2 * r2);
+        //u = fx*Xdis +cx, v = fy+Ydis + cy
         return Vector2d(_estimate.focal * distortion * pc[0],
                         _estimate.focal * distortion * pc[1]);
     }
@@ -76,6 +83,10 @@ public:
     virtual bool write(ostream &out) const {}
 };
 
+/*优化节点路标点 
+ * @param 3 路标点的维数
+ * @param Vector3d 路标点的数据结构
+ */
 class VertexPoint : public g2o::BaseVertex<3, Vector3d> {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
@@ -95,6 +106,13 @@ public:
     virtual bool write(ostream &out) const {}
 };
 
+//定义图优化边
+//class EdgeProjection :public g2o::BaseBinaryEdge<2, Vector2d, VertexPoseAndIntrinsics, VertexPoint>
+//这里没有设置linearizeOplus函数，即没有设置雅可比矩阵，所以采用G2O内置的数值求导
+//@param 2:边，观测的维数
+//@param Vector2d：观测量的数据结构
+//@param VertexPoseAndIntrinsics：优化节点，包含旋转矩阵，平移，相机内参
+//@param VertexPoint ：优化节点2，路标点
 class EdgeProjection :
     public g2o::BaseBinaryEdge<2, Vector2d, VertexPoseAndIntrinsics, VertexPoint> {
 public:
@@ -103,7 +121,9 @@ public:
     virtual void computeError() override {
         auto v0 = (VertexPoseAndIntrinsics *) _vertices[0];
         auto v1 = (VertexPoint *) _vertices[1];
+        //计算投影后的像素点
         auto proj = v0->project(v1->estimate());
+        //投影误差
         _error = proj - _measurement;
     }
 
@@ -134,69 +154,81 @@ int main(int argc, char **argv) {
 }
 
 void SolveBA(BALProblem &bal_problem) {
+    //获取参数的地址
     const int point_block_size = bal_problem.point_block_size();
     const int camera_block_size = bal_problem.camera_block_size();
     double *points = bal_problem.mutable_points();
     double *cameras = bal_problem.mutable_cameras();
 
-    // pose dimension 9, landmark is 3
+    //优化的参数分别是9维度的pose和3维度的路标点
     typedef g2o::BlockSolver<g2o::BlockSolverTraits<9, 3>> BlockSolverType;
+    //定义线性求解器类型
     typedef g2o::LinearSolverCSparse<BlockSolverType::PoseMatrixType> LinearSolverType;
-    // use LM
+    //使用LM方法
     auto solver = new g2o::OptimizationAlgorithmLevenberg(
         g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+    //图模型
     g2o::SparseOptimizer optimizer;
+    //设置求解器
     optimizer.setAlgorithm(solver);
+    //打开调试
     optimizer.setVerbose(true);
 
-    /// build g2o problem
+    //建立g2o问题
     const double *observations = bal_problem.observations();
     // vertex
     vector<VertexPoseAndIntrinsics *> vertex_pose_intrinsics;
     vector<VertexPoint *> vertex_points;
+    //添加节点
     for (int i = 0; i < bal_problem.num_cameras(); ++i) {
         VertexPoseAndIntrinsics *v = new VertexPoseAndIntrinsics();
         double *camera = cameras + camera_block_size * i;
-        v->setId(i);
-        v->setEstimate(PoseAndIntrinsics(camera));
-        optimizer.addVertex(v);
-        vertex_pose_intrinsics.push_back(v);
+        v->setId(i);//设置顶点ID
+        v->setEstimate(PoseAndIntrinsics(camera));//设置顶点估计值
+        optimizer.addVertex(v);//往优化器添加顶点
+        vertex_pose_intrinsics.push_back(v);//往矩阵添加节点
     }
     for (int i = 0; i < bal_problem.num_points(); ++i) {
-        VertexPoint *v = new VertexPoint();
+        VertexPoint *v = new VertexPoint();//创建新的路标点
         double *point = points + point_block_size * i;
-        v->setId(i + bal_problem.num_cameras());
-        v->setEstimate(Vector3d(point[0], point[1], point[2]));
-        // g2o在BA中需要手动设置待Marg的顶点
+        v->setId(i + bal_problem.num_cameras());//设置ID
+        v->setEstimate(Vector3d(point[0], point[1], point[2]));//
+        // g2o在BA中需要手动设置待Marg的顶点，即在优化过程，该节点需要边缘化
         v->setMarginalized(true);
         optimizer.addVertex(v);
         vertex_points.push_back(v);
     }
 
-    // edge
+    //添加边
     for (int i = 0; i < bal_problem.num_observations(); ++i) {
         EdgeProjection *edge = new EdgeProjection;
+        //给二元边添加两个节点，是观测量对应的两个节点
         edge->setVertex(0, vertex_pose_intrinsics[bal_problem.camera_index()[i]]);
         edge->setVertex(1, vertex_points[bal_problem.point_index()[i]]);
+        //设置观测量
         edge->setMeasurement(Vector2d(observations[2 * i + 0], observations[2 * i + 1]));
+        //观测数值
         edge->setInformation(Matrix2d::Identity());
+        //设置鲁内核
         edge->setRobustKernel(new g2o::RobustKernelHuber());
+        //添加边
         optimizer.addEdge(edge);
     }
 
     optimizer.initializeOptimization();
+    //迭代40次
     optimizer.optimize(40);
 
     // set to bal problem
     for (int i = 0; i < bal_problem.num_cameras(); ++i) {
         double *camera = cameras + camera_block_size * i;
         auto vertex = vertex_pose_intrinsics[i];
-        auto estimate = vertex->estimate();
-        estimate.set_to(camera);
+        auto estimate = vertex->estimate();//获取位姿的最优值9维 
+        estimate.set_to(camera);//将优化后的值覆盖原先的值
     }
     for (int i = 0; i < bal_problem.num_points(); ++i) {
         double *point = points + point_block_size * i;
-        auto vertex = vertex_points[i];
-        for (int k = 0; k < 3; ++k) point[k] = vertex->estimate()[k];
+        auto vertex = vertex_points[i];//获得最佳的路标点
+        for (int k = 0; k < 3; ++k) point[k] = vertex->estimate()[k];//将路标点覆盖
     }
 }
